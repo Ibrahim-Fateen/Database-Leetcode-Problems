6b94c6f294662a9f758f55e3d9418caaa79818e0
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-09-04 17:27:35 +0300

    Wrong problem description

--- Product Sales Analysis.sql
+++ Product Sales Analysis.sql
@@ -1,7 +1,7 @@
 -- Write your PostgreSQL query statement below
 SELECT s1.product_id, s1.year AS first_year, s1.quantity, s1.price FROM SALES s1
-WHERE (s1.sales_id, s1.year) = (
-    SELECT s2.sales_id, s2.year FROM SALES s2
+WHERE (s1.product_id, s1.year) = (
+    SELECT s2.product_id, s2.year FROM SALES s2
     WHERE s2.product_id = s1.product_id
     ORDER BY s2.year ASC LIMIT 1
 ); -- WRONG ANSWER :O
