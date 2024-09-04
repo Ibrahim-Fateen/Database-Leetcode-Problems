6209139dc3dabdbef90797b4deff633d76176ab5
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-09-04 17:50:29 +0300

    Second Try, Passes :)

--- Market Analysis.sql
+++ Market Analysis.sql
@@ -1,7 +1,11 @@
 -- Write your PostgreSQL query statement below
 SELECT u.user_id AS buyer_id, u.join_date AS join_date, (
-    COUNT(*)
+    COUNT(
+            CASE
+                WHEN EXTRACT(YEAR FROM o.order_date) = '2019'
+                    THEN 1
+                END
+    )
     ) AS orders_in_2019
 FROM Users u LEFT OUTER JOIN Orders o ON u.user_id = o.buyer_id
-WHERE EXTRACT(Year FROM order_date) = '2019'
 GROUP BY u.user_id, u.join_date;
